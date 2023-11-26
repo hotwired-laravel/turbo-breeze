2b2c8f343481b60f42cf10e85cf02144d64927f9
Tony Messias <tonysm@hey.com>
2023-11-26 11:57:20 -0300

    Split up in partials

--- stubs/turbo/resources/views/profile/delete.blade.php
+++ stubs/turbo/resources/views/profile/delete.blade.php
@@ -9,35 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <section class="space-y-6">
-                        <header>
-                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                                {{ __('Delete Account') }}
-                            </h2>
-
-                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
-                            </p>
-                        </header>
-
-                        <form method="post" action="{{ route('profile.destroy') }}">
-                            @csrf
-                            @method('delete')
-
-                            <div>
-                                <x-input-label for="password" :value="__('Password')" />
-                                <x-text-input id="password" name="password" type="password" class="mt-1 block w-full"
-                                    required autofocus />
-                                <x-input-error class="mt-2" :messages="$errors->get('password')" />
-                            </div>
-
-                            <div class="mt-6 flex justify-end">
-                                <x-danger-button class="ms-3">
-                                    {{ __('Delete Account') }}
-                                </x-danger-button>
-                            </div>
-                        </form>
-                    </section>
+                    @include('profile.partials.delete-user-form')
                 </div>
             </div>
         </div>

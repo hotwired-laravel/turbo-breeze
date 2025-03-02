5720126a62a96a89ea6fcd0441ec25d91ad51538
Tony Messias <tonysm@hey.com>
2025-03-02 18:55:17 -0300

    Tweaks

--- src/Console/InstallsTurboStack.php
+++ src/Console/InstallsTurboStack.php
@@ -104,6 +104,8 @@ trait InstallsTurboStack
             // NPM Packages...
             $this->updateNodePackages(function ($packages) {
                 return [
+                    '@tailwindcss/vite' => '^4.0',
+                    'autoprefixer' => '^10.4.20',
                     'tailwindcss' => '^4.0',
                     'el-transition' => '^0.0.7',
                 ] + $packages;
